6502d10acc8a543583ca71be3057374de37bcd4c
Shubham Soni <dev.shubhamsoni@gmail.com>
2026-05-12 22:24:08 +0530

    changes

--- src/email/email-recipients.ts
+++ src/email/email-recipients.ts
@@ -1,7 +1,11 @@
 export const EMAIL_RECIPIENTS = {
   COMMON_CONTETRA: [
-    'shubhams000ni@gmail.com',
     'shubham.soni@contetra.com',
     'pankaj.sakpal@contetra.com',
+    'tejas.savla@contetra.com',
+    'chitra.parameswaran@contetra.com',
+    'aastha.varshneya@contetra.com',
+    'palak.kedia@contetra.com',
+    'kashish.rajpal@contetra.com',
   ],
 };
